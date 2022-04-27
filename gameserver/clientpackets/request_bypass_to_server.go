a89342d6e9a6fff9114d23892b626c0bc8b9cce1
Cannabytes <logan22f@gmail.com>
2022-04-27 11:33:18 +0300

    new html buff community

--- gameserver/clientpackets/request_bypass_to_server.go
+++ gameserver/clientpackets/request_bypass_to_server.go
@@ -1,7 +1,9 @@
 package clientpackets
 
 import (
+	inter "l2gogameserver/data"
 	"l2gogameserver/data/logger"
+	buff2 "l2gogameserver/gameserver/buff"
 	"l2gogameserver/gameserver/community"
 	"l2gogameserver/gameserver/interfaces"
 	"l2gogameserver/gameserver/models"
@@ -20,7 +22,7 @@ import (
 	Все запросы на открытие страницы будут начинатся с _bbspage
 	следующие параметры разделены двоеточием:
 	[вызов страницы]:[команда]:[информация]:[информация]:[информация]...
-	_bbspage:open:/page/index.htm (аналог _bbspage:open:page) - открыть файл
+	_bbspage:open:/page/index.htm (аналог _bbspage:open:page) - открыть файл (bypass -h _bbspage:open:buffer/buffs.htm)
 	_bbspage:buffer:combo:3 - наложение комбо баффа с ID 3
 	_bbspage:buffer:save - сохранить бафф персонажа
 	_bbspage:buffer:get:3 - бафф персонажа (ранее сохраненным баффом) с ID 3
@@ -31,8 +33,12 @@ import (
 	_bbspage:teleport:to:5 - телепорт ранее сохраненную позицию с ID 5
 	_bbspage:teleport:remove:5 - удаление сохраненной точки с ID 5
 	...
+	// Функция наложения баффов
+	_bbsbuff:1204:2:0:page/index.htm - где ID баффа 1204 и уровень 2 и цена 0, следующим параметром отправляем данные о том какую страницу открыть
 */
-func BypassToServer(data []byte, client interfaces.ReciverAndSender) {
+func BypassToServer(data []byte, clientI interfaces.ReciverAndSender) {
+	client := clientI.(*models.Client).CurrentChar
+
 	var bypassRequest = packets.NewReader(data).ReadString()
 	bypassInfo := strings.Split(bypassRequest, ":")
 	for i, s := range bypassInfo {
@@ -41,14 +47,13 @@ func BypassToServer(data []byte, client interfaces.ReciverAndSender) {
 	logger.Info.Println(bypassInfo)
 	if bypassInfo[0] == "_bbshome" && bypassRequest == "_bbshome" {
 		//Открытие диалога по умолчанию
-		SendOpenDialogBBS(client, "./datapack/html/community/index.htm")
+		SendOpenDialogBBS(clientI, "./datapack/html/community/index.htm")
 	} else if bypassInfo[0] == "_bbspage" {
 		commandname := bypassInfo[1]
 		switch commandname {
 		//Запрос открытия диалога
 		case "open":
-			SendOpenDialogBBS(client, "./datapack/html/community/"+bypassInfo[2])
-
+			SendOpenDialogBBS(clientI, "./datapack/html/community/"+bypassInfo[2])
 		//Функции телепортации
 		case "teleport":
 			switch bypassInfo[2] {
@@ -58,8 +63,8 @@ func BypassToServer(data []byte, client interfaces.ReciverAndSender) {
 					logger.Info.Println(err)
 					return
 				}
-				pkg := community.UserTeleport(client, teleportID)
-				client.EncryptAndSend(pkg)
+				pkg := community.UserTeleport(clientI, teleportID)
+				clientI.EncryptAndSend(pkg)
 			case "save":
 				logger.Info.Println("Сохранение позиции игрока")
 			case "to":
@@ -67,7 +72,6 @@ func BypassToServer(data []byte, client interfaces.ReciverAndSender) {
 			case "remove":
 				logger.Info.Println("Удаление по сохраненной позиции игрока #", bypassInfo[3])
 			}
-
 		case "gmshop":
 			switch bypassInfo[2] {
 			case "multisell": //Open multisell
@@ -77,14 +81,33 @@ func BypassToServer(data []byte, client interfaces.ReciverAndSender) {
 					return
 				}
 				logger.Info.Println("Открыть мультиселл с ID", id)
-				multisellList, ok := multisell.Get(client, id)
+				multisellList, ok := multisell.Get(clientI, id)
 				if !ok {
 					logger.Info.Println("Не найден запрашиваемый мультисел#")
 				}
 				pkg := serverpackets.MultiSell(multisellList)
-				client.EncryptAndSend(pkg)
+				clientI.EncryptAndSend(pkg)
 			}
+		}
 
+	} else if bypassInfo[0] == "_bbsbuff" {
+		//Функция наложения баффа на персонажа
+		buffId := bypassInfo[1]
+		buffLevel := bypassInfo[2]
+		buffCost := bypassInfo[3]
+		_ = buffCost
+		client.Buff = append(client.Buff, &models.BuffUser{
+			Id:     inter.StrToInt(buffId),
+			Level:  inter.StrToInt(buffLevel),
+			Second: 60,
+		})
+		buff2.ComparisonBuff(client.Conn)
+		pkg17 := serverpackets.AbnormalStatusUpdate(client.Buff)
+		client.EncryptAndSend(pkg17)
+
+		if len(bypassInfo) == 5 {
+			page := bypassInfo[4]
+			SendOpenDialogBBS(clientI, "./datapack/html/community/"+page)
 		}
 
 	}
